96e19d4a394c3310c65758ff32148c6a09263800
Guadalupe-Cano <lupita082908@icloud.com>
2025-09-15 22:03:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 print("Welcome to Elite101 Chat Bot!")
 first_name = input("Please enter your first name: ")
 last_name = input("Please enter your last name: )
-user_age = int(input("How old are you? "))
-if user_age <=12:
+user_age = int(input("Pleasure to meet you " + first_name + "! How old are you? "))
+
+#if user_age <=12:
+#print("Welcome
 
 
 
