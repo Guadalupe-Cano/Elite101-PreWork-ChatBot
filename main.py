d47493faf50f6d3ec3e091283f37708622ba87f8
Guadalupe-Cano <lupita082908@icloud.com>
2025-09-20 00:43:29 +0000

    Elite101-PreWork-Chatbot

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ print("Welcome to Elite101 Chat Bot!")
 first_name = input("Please enter your first name: ")
 last_name = input("Please enter your last name: ")
 user_age = int(input("Pleasure to meet you " + first_name + "! How old are you? "))
-print("WoW you're "+ user_age +" years old! Welcome! How can I assist you today? ")
-
+print("WoW you're "+ str(user_age) +" years old! Welcome! How can I assist you today? ")
+print(" ")
 print("Please choose from the following options! ")
 print("1. learn about AI ")
 print("2. Environemental consequences of AI ")
@@ -36,11 +36,14 @@ if user_options == 5:
   
 if user_options == 6:
   print(" GoodBye " + first_name+ "! Hope you have a great day!")
-  break
+    
 
 else:
-print("Invalid! Try again")
-user_options = int(input("Enter an option number: "))
+  print("Invalid! Try again")
+  user_options = int(input("Enter an option number: "))
+
+
+
 
 
 
